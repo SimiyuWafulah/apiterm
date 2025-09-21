de1c90074661913d33e6d5b2d1760db68357ead9
Kelvin Simiyu Wafulah <simiyukelvinse@gmail.com>
2025-09-21 16:36:26 +0300

    feat: add post command for post requests with json payloads

--- internal/httpclient.go
+++ internal/httpclient.go
@@ -6,6 +6,7 @@ import (
     "net/http"
 )
 
+//get logic for get requests
 func Get(url string) (int, []byte, error) {
     resp, err := http.Get(url)
     if err != nil {
@@ -19,4 +20,20 @@ func Get(url string) (int, []byte, error) {
     }
 
     return resp.StatusCode, body, nil
+}
+
+//post requests
+func Post(url string, body []byte) (int, []byte, error) {
+    resp, err := http.Post(url, "application/json", bytes.NewBuffer(body))
+    if err != nil {
+        return 0, nil, err
+    }
+    defer resp.Body.Close()
+
+    respBody, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return resp.StatusCode, nil, err
+    }
+
+    return resp.StatusCode, respBody, nil
 }
