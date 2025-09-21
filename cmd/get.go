219da0abb6bb3fb0934a265b53c9e91267ed726a
Kelvin Simiyu Wafulah <simiyukelvinse@gmail.com>
2025-09-21 16:30:20 +0300

    re

--- cmd/get.go
+++ cmd/get.go
@@ -2,39 +2,22 @@ package cmd
 
 import (
     "fmt"
-    "io/ioutil"
-    "net/http"
-
+    "github.com/SimiyuWafulah/apiterm/internal"
     "github.com/spf13/cobra"
 )
 
-// Defines a new Cobra command 
 var getCmd = &cobra.Command{
     Use:   "get [url]",
-    Short: "Send get request to any URL",
+    Short: "Send a GET request to the specified URL",
     Args:  cobra.MinimumNArgs(1),
     Run: func(cmd *cobra.Command, args []string) {
         url := args[0]
-        resp, err := http.Get(url)
+        status, body, err := internal.Get(url)
         if err != nil {
             fmt.Println("Request error:", err)
             return
         }
-        defer resp.Body.Close()
-
-        // Reads the entire response body
-        body, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-            fmt.Println("Error reading response:", err)
-            return
-        }
-
-        fmt.Println("Status:", resp.Status)
+        fmt.Println("Status:", status)
         fmt.Println(string(body))
     },
-}
-
-// Registers get  with the root command for use in the cli
-func init() {
-    rootCmd.AddCommand(getCmd)
 }
