cf40e05ae64b2a1b7e29e624ad5222c3a2eecc0e
Kelvin Simiyu Wafulah <simiyukelvinse@gmail.com>
2025-09-21 19:49:17 +0300

    chore:update root command help text to include apiterm option

--- cmd/root.go
+++ cmd/root.go
@@ -1,23 +1,24 @@
 package cmd
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 
-    "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-    Use:   "apiterm",
-    Short: "A CLI tool to test APIs from the terminal",
-    Run: func(cmd *cobra.Command, args []string) {
-        fmt.Println("Welcome to apiterm, Enter help for available commands.")
-    },
+	Use:   "apiterm",
+	Short: "A CLI tool to test APIs from the terminal",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Welcome to apiterm. Use 'get', 'post', or 'apiterm' commands.")
+		fmt.Println("Enter 'apiterm help' for available commands.")
+	},
 }
 
 func Execute() {
-    if err := rootCmd.Execute(); err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
